c9caef207a664ee93afb38427c4f1f803c06a1e0
JudahPraise <judahpraiseocampo@gmail.com>
2021-04-13 23:23:06 +0800

    Map Completed

---
+++ resources/views/components/maps.blade.php
@@ -0,0 +1,37 @@
+@if($barangay == 'Balayhangin')
+    <iframe class="card-img-top" class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19647.624173615866!2d121.30719958644887!3d14.131945249993219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5c36ee0dd935%3A0x27d760ceed7bf0e3!2sBalayhangin%2C%20Brgy%20San%20Lucas%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1617810050349!5m2!1sen!2sph"  allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Bangyas')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19643.066188547433!2d121.30269878645348!3d14.184641494624394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5ef921d2e9f7%3A0xd3c7bf7b28f74744!2sBangyas%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326487126!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Dayap')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19643.323062709485!2d121.32105048645323!3d14.181676794926902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5ee714badb33%3A0x12f21f6aa3b9f120!2sDayap%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326548326!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Dayap II - NHA')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4910.943582617847!2d121.33681791536262!3d14.176467041128829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5ecf97594acf%3A0x6e90cb157f8b0c10!2sNHA%20Calauan!5e1!3m2!1sen!2sph!4v1618326596771!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Hanggan')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19642.873747577807!2d121.29112233645368!3d14.18686214439776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5e5177e8b441%3A0xf4301211ef98c64e!2sHanggan%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326641052!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Imok')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19648.649586115003!2d121.29877203644783!3d14.12006360120139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5db2a5ab822d%3A0xd096d5568816dfef!2sImok%2C%20Brgy%20San%20Lucas%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326696344!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Lamot 1')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19646.865811065414!2d121.31935533644966!3d14.140726249099764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5c23b3f9b58b%3A0x872858239695562b!2sLamot%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326726251!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Lamot 2')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39291.36518048776!2d121.31389960613969!3d14.154415978376392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5ea278cce691%3A0x85ea207a8f2dc94d!2sLamot%202%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326791284!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Limao')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39301.63116427049!2d121.22332680611856!3d14.094933541695715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd67ea1e9c128f%3A0x469a998605df122f!2sLimao%2C%20San%20Vicente%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326828063!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Mabacan')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19647.252265718347!2d121.27670278644922!3d14.136252199555047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5dea78195815%3A0xa153f22ccba848ab!2sMabacan%2C%20Brgy%20San%20Lucas%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326862659!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Masiit')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19645.220040832337!2d121.2965940864513!3d14.15976409716109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5e7ce0818ca7%3A0x89f042a31ae6a44e!2sMasiit%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326898351!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Paliparan')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19649.048958063675!2d121.27676188644739!3d14.115433351671978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5d9f7df67781%3A0xfc32d36c6796c6d3!2sPaliparan%2C%20Brgy%20San%20Lucas%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326931020!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Perez')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19649.542409727354!2d121.26077053644688!3d14.109710302253442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd678743417647%3A0x76a2af28431dc697!2sPerez%2C%20Brgy%20San%20Lucas%201%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618326967970!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Prinza')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19647.18662770681!2d121.31535878644928!3d14.137012199477725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5c3b387a107b%3A0x4afbd39b07397585!2sPrinza%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618327003051!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Kanluran')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4911.620148486976!2d121.31111566536246!3d14.145184591865453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5c2befc0aefb%3A0x9f67815848bd7d5c!2sKanluran%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618327031579!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Silangan')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4911.58395711059!2d121.31232266536227!3d14.146859691826043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5c2978a9fceb%3A0x58652e1c5e2fc388!2sSilangan%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618327131877!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'San Isidro')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19645.12149131977!2d121.3098335364514!3d14.160903297045008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5e8fc7005377%3A0x79076d893c41e1c!2sSan%20Isidro%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618327164324!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@elseIf($barangay == 'Santo Tomas')
+    <iframe class="card-img-top" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19645.193929255067!2d121.33918763645133!3d14.16006594713033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5eb6acd22d57%3A0xa12632c66beae41c!2sSanto%20Tomas%2C%20Calauan%2C%20Laguna!5e1!3m2!1sen!2sph!4v1618327197789!5m2!1sen!2sph" allowfullscreen="" loading="lazy"></iframe>
+@endif
--- resources/views/survey/home.blade.php
+++ resources/views/survey/home.blade.php
@@ -3,19 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
                     {{ session('status') }}
                 </div>
             @endif
 
-            @forelse ($barangays as $barangay)
-                 <p>{{ $barangay->barangay }}</p>
-            @empty
-                 <p>No data</p>
-            @endforelse
-
+            <div class="card-columns">
+                @foreach ($barangays as $barangay)
+                <div class="card">
+                    @component('components.maps', ['barangay' => $barangay->barangay])@endcomponent
+                  <div class="card-body">
+                    <h2 class="card-title">{{ $barangay->barangay }}</h2>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
