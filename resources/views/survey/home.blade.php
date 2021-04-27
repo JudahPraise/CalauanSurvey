cbd422f1286bc150a5f88560f63637295d35964d
JudahPraise <judahpraiseocampo@gmail.com>
2021-04-27 21:52:09 +0800

    "Home Fixed"

--- resources/views/survey/home.blade.php
+++ resources/views/survey/home.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<<<<<<< HEAD
+
       <div class="container-fluid">
         @yield('survey-home')
     </div>
->>>>>>> 5731d80e97681a503c136eb8e6f2cdfe1f843a65
+
 @endsection
 
 {{-- Rnwck Brent--}}
