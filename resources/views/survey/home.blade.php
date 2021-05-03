918c16e177277efadf13a29d97259f5d633303b7
qwebrent <qwebrent@gmail.com>
2021-05-04 07:46:53 +0800

    Update

--- resources/views/survey/home.blade.php
+++ resources/views/survey/home.blade.php
@@ -2,22 +2,8 @@
 
 @section('content')
 
-
-      <div class="container-fluid">
+    <div class="container-fluid">
         @yield('survey-home')
-            <div class="card-columns">
-                @foreach ($barangays as $barangay)
-                <div class="card">
-                    @component('components.maps', ['barangay' => $barangay->barangay])@endcomponent
-                  <div class="card-body">
-                    <h2 class="card-title">{{ $barangay->barangay }}</h2>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
     </div>
 
 @endsection
